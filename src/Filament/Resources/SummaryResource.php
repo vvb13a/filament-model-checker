70e724f4554cc1aadc0505dab0afa062369ca61d
vvb-413 <vvb@wiot-tomorrow.com>
2025-05-06 15:15:44 +0200

    Scope SummaryResource Eloquent Query

--- src/Filament/Resources/SummaryResource.php
+++ src/Filament/Resources/SummaryResource.php
@@ -2,7 +2,10 @@
 
 namespace Vvb13a\FilamentModelChecker\Filament\Resources;
 
+use Filament\Facades\Filament;
 use Filament\Resources\Resource;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Vvb13a\FilamentModelChecker\Filament\Resources\SummaryResource\Pages\ListSummaries;
 use Vvb13a\LaravelModelChecker\Models\Summary;
 
@@ -17,5 +20,27 @@ class SummaryResource extends Resource
         return [
             'index' => ListSummaries::route('/'),
         ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        $currentPanel = Filament::getCurrentPanel();
+        $plugin = null;
+
+        if ($currentPanel && $currentPanel->hasPlugin('filament-model-checker')) {
+            $plugin = $currentPanel->getPlugin('filament-model-checker');
+        }
+
+        $checkableTypes = $plugin?->getCheckableTypes() ?? [];
+
+        if (empty($checkableTypes)) {
+            return parent::getEloquentQuery();
+        }
+
+        $aliases = collect($checkableTypes)->map(function ($value) {
+            return Relation::getMorphAlias($value);
+        })->toArray();
+
+        return parent::getEloquentQuery()->whereIn('checkable_type', $aliases);
     }
 }
